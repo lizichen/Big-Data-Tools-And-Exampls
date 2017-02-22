0b81b8db1f94d6562d00cc53f73358e6c04d6e6d
cloudera <cloudera@quickstart.cloudera>
2017-02-22 10:23:31 -0800

    fix bug

--- PageRank/src/PageRankMapper.java
+++ PageRank/src/PageRankMapper.java
@@ -19,15 +19,16 @@ public class PageRankMapper extends Mapper<LongWritable, Text, Text, Text> {
 		String outlinks = "";
 		
 		String from = inputArr[0];
-		for(int i = 1 ; i < length-1 ; i++){									// A C F 0.166667 - loop from C to F
+		for(int i = 1 ; i < length-1 ; i++){														// A C F 0.166667 - loop from C to F
 			String to = inputArr[i];
-			PR = Float.parseFloat(inputArr[length-1]) / (length-2); 			// number of outlink destinations
+			PR = Float.parseFloat(inputArr[length-1]) / (length-2); 								// number of outlink destinations
 			context.write(new Text(to), new Text(from + PR_VALUE_SPECIAL_TOKEN + PR.toString()));	// produce key-value map pair: {C, A#0.833335}
-			outlinks.concat(to+" ");
+			//outlinks.concat(to+" ");
+			outlinks += (to+" ");
 		}
-		context.write(new Text(from), new Text(outlinks.trim()));				// produce key-value map pair: {A, C F}
+		context.write(new Text(from), new Text(outlinks.trim()));									// produce key-value map pair: {A, C F}
 		// logging
-		System.out.println("key="+from+" value="+outlinks);
+		//System.out.println("key="+from+" value="+outlinks);
 		
 		
 	}
